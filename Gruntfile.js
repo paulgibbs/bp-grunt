eb8c325871f00951ae24e93935d8c43ad032cc7c
Paul Gibbs <paul@byotos.com>
2014-01-25 23:52:57 +0100

    First pass at Grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,63 @@
-/*global module:false*/
-module.exports = function(grunt) {
-	var path = require('path');
-	var SOURCE_DIR = 'src/';
-	var BUILD_DIR = 'build/';
+/* jshint node:true */
+module.exports = function( grunt ) {
+	var path = require('path'),
+	SOURCE_DIR = 'src/',
+	BUILD_DIR      = 'build/',
+
+	// RTL from cssjanus
+	BP_RTL_CSS = [
+		'bp-activity/admin/css/*-rtl.css',
+		'bp-core/admin/css/*-rtl.css',
+		'bp-core/css/*-rtl.css',
+		'bp-groups/admin/css/*-rtl.css',
+		'bp-messages/css/*-rtl.css',
+		'bp-templates/bp-legacy/css/*-rtl.css',
+		'bp-xprofile/admin/css/*-rtl.css'
+	],
+	BP_LTR_CSS = [
+		'bp-activity/admin/css/*.css',
+		'bp-core/admin/css/*.css',
+		'bp-core/css/*.css',
+		'bp-groups/admin/css/*.css',
+		'bp-messages/css/*.css',
+		'bp-templates/bp-legacy/css/*.css',
+		'bp-xprofile/admin/css/*.css'
+	],
+
+	BP_JS = [
+		'bp-activity/admin/js/*.js',
+		'bp-core/js/*.js',
+		'bp-friends/js/*.js',
+		'bp-groups/admin/js/*.js',
+		'bp-groups/js/*.js',
+		'bp-messages/js/*.js',
+		'bp-templates/bp-legacy/js/*.js',
+		'bp-xprofile/admin/js/*.js'
+	];
 
 	// Load tasks.
-	require('matchdep').filterDev('grunt-*').forEach( grunt.loadNpmTasks );
+	require( 'matchdep' ).filterDev( 'grunt-*' ).forEach( grunt.loadNpmTasks );
 
 	// Project configuration.
 	grunt.initConfig({
 		clean: {
 			all: [BUILD_DIR],
-			build: [BUILD_DIR + 'templates/achievements/css/dev/',
-							BUILD_DIR + 'includes/admin/css/dev/'],
 			dynamic: {
 				cwd: BUILD_DIR,
 				dot: true,
 				expand: true,
-				src: [],
+				src: []
 			}
 		},
 		copy: {
-			all: {
+			files: {
 				files: [
 					{
 						cwd: SOURCE_DIR,
 						dest: BUILD_DIR,
 						dot: true,
 						expand: true,
-						src: ['!**/.{svn,git}/**','**'], // Ignore version control directories.
+						src: [ '!**/.{svn,git}/**', '**' ] // Ignore version control directories.
 					}
 				]
 			},
@@ -37,22 +66,7 @@ module.exports = function(grunt) {
 				dest: BUILD_DIR,
 				dot: true,
 				expand: true,
-				src: [],
-			}
-		},
-		less: {
-			core: {
-				files: {
-					// front-end
-					'src/templates/achievements/css/achievements.css':  'src/templates/achievements/css/dev/achievements.less',
-
-					// admin
-					'src/includes/admin/css/achievements.css':     'src/includes/admin/css/dev/achievements.less',
-					'src/includes/admin/css/admin-editindex.css':  'src/includes/admin/css/dev/admin-editindex.less',
-					'src/includes/admin/css/chosen.css':           'src/includes/admin/css/dev/chosen.less',
-					'src/includes/admin/css/supportedplugins.css': 'src/includes/admin/css/dev/supportedplugins.less',
-					'src/includes/admin/css/users.css':            'src/includes/admin/css/dev/users.less',
-				}
+				src: []
 			}
 		},
 		cssmin: {
@@ -60,28 +74,17 @@ module.exports = function(grunt) {
 				cwd: SOURCE_DIR,
 				dest: BUILD_DIR,
 				expand: true,
-				ext: '.css',
-				src: [
-					'templates/achievements/css/achievements.css',
-					'includes/admin/css/{achievements,admin-editindex,chosen,supportedplugins,users}.css'
-				],
-				options: {
-					banner: '/*! http://wordpress.org/plugins/achievements/ */'
-				}
+				ext: '.min.css',
+				src: BP_LTR_CSS,
+				options: { banner: '/*! https://wordpress.org/plugins/buddypress/ */' }
 			},
 			rtl: {
 				cwd: BUILD_DIR,
 				dest: BUILD_DIR,
 				expand: true,
-				ext: '.css',
-				src: [
-					// RTL from cssjanus
-					'templates/achievements/css/achievements-rtl.css',
-					'includes/admin/css/{achievements,admin-editindex,chosen,supportedplugins,users}-rtl.css'
-				],
-				options: {
-					banner: '/*! http://wordpress.org/plugins/achievements/ */'
-				}
+				ext: '.min.css',
+				src: BP_RTL_CSS,
+				options: { banner: '/*! https://wordpress.org/plugins/buddypress/ */' }
 			}
 		},
 		cssjanus: {
@@ -90,12 +93,51 @@ module.exports = function(grunt) {
 				cwd: SOURCE_DIR,
 				dest: BUILD_DIR,
 				ext: '-rtl.css',
-				src: [
-					'templates/achievements/css/achievements.css',
-					'includes/admin/css/{achievements,admin-editindex,chosen,supportedplugins,users}.css'
-				],
-				options: {
-					generateExactDuplicates: true
+				src: BP_LTR_CSS,
+				options: { generateExactDuplicates: true }
+			},
+			dynamic: {
+				expand: true,
+				cwd: SOURCE_DIR,
+				dest: BUILD_DIR,
+				ext: '-rtl.css',
+				src: []
+			}
+		},
+		jshint: {
+			options: grunt.file.readJSON( '.jshintrc' ),
+			grunt: {
+				src: [ 'Gruntfile.js' ]
+			},
+			core: {
+				expand: true,
+				cwd: SOURCE_DIR,
+				src: BP_JS,
+
+				/**
+				 * Limit JSHint's run to a single specified file: grunt jshint:core --file=filename.js
+				 *
+				 * @param {String} filepath
+				 * @returns {Bool}
+				 */
+				filter: function( filepath ) {
+					var index, file = grunt.option( 'file' );
+
+					// Don't filter when no target file is specified
+					if ( ! file ) {
+						return true;
+					}
+
+					// Normalise filepath for Windows
+					filepath = filepath.replace( /\\/g, '/' );
+					index = filepath.lastIndexOf( '/' + file );
+
+					// Match only the filename passed from cli
+					if ( filepath === file || ( -1 !== index && index === filepath.length - ( file.length + 1 ) ) ) {
+						return true;
+					}
+
+					return false;
 				}
 			}
 		},
@@ -104,59 +146,96 @@ module.exports = function(grunt) {
 				cwd: SOURCE_DIR,
 				dest: BUILD_DIR,
 				expand: true,
-				ext: '.js',
-				src: [
-					'includes/admin/js/achievements.js',
-					'includes/admin/js/supportedplugins.js',
-					'templates/achievements/js/*.js'
-				],
+				ext: '.min.js',
+				src: BP_JS
 			},
 			options: {
-				banner: '/*! http://wordpress.org/plugins/achievements/ */\n'
+				banner: '/*! https://wordpress.org/plugins/buddypress/ */\n'
 			}
 		},
 		phpunit: {
-			all: {
-				dir: 'tests/phpunit/'
+			'default': {
+				cmd: 'phpunit',
+				args: ['-c', 'phpunit.xml']
+			},
+			multisite: {
+				cmd: 'phpunit',
+				args: ['-c', 'tests/phpunit/multisite.xml']
+			}
+		},
+		jsvalidate:{
+			options:{
+				globals: {},
+				esprimaOptions:{},
+				verbose: false
+			},
+			build: {
+				files: {
+					src: BUILD_DIR + '/**/*.js'
+				}
 			}
 		},
 		watch: {
-			css: {
-				files: ["src/templates/achievements/css/dev/*",
-								"src/includes/admin/css/dev/*"],
-				tasks: ["less", "rsync:everything"],
+			all: {
+				files: [
+					SOURCE_DIR + '**',
+					// Ignore version control directories.
+					'!' + SOURCE_DIR + '**/.{svn,git}/**'
+				],
+				tasks: [ 'clean:dynamic', 'copy:dynamic' ],
 				options: {
+					dot: true,
+					interval: 2000,
 					spawn: false
 				}
 			},
-			sync: {
-				files: ['src/**'],
-				tasks: ["rsync:everything"],
+			rtl: {
+				files: BP_LTR_CSS.map( function( path ) {
+					return SOURCE_DIR + path;
+				} ),
+				tasks: [ 'cssjanus:dynamic' ],
 				options: {
+					interval: 2000,
 					spawn: false
-				}
-			}
-		},
-		rsync: {
-			options: {
-				exclude: ['.git*', '.DS_Store*', '._*', '.Spotlight*', '.Trashes*', '*thumbs.db*'],
-				recursive: true,
-				spawn: false,
-				syncDestIgnoreExcl: true
-			},
-			everything: {
-				options: {
-					src: "src/",
-					dest: process.env.ACHIEVEMENTS_DIR
 				}
 			}
 		}
 	});
 
-	// Register tasks.
-	grunt.registerTask('build-dev',  ['clean:all', 'less:core']);
-	grunt.registerTask('build-prod', ['clean:all', 'less:core', 'copy:all', 'cssjanus:core', 'cssmin:ltr', 'cssmin:rtl', 'uglify:core', 'clean:build', 'phpunit:all']);
+
+	// Build tasks.
+	grunt.registerTask( 'build', [ 'clean:all', 'copy:all', 'cssjanus:core', 'cssmin:ltr', 'cssmin:rtl', 'uglify:core', 'clean:build', 'jsvalidate:build' ] );
+	grunt.registerTask( 'build-release', [ 'clean:all', 'copy:all', 'cssjanus:core', 'cssmin:ltr', 'cssmin:rtl', 'uglify:core', 'clean:build', 'jsvalidate:build', 'phpunit:all' ] );
+
+	// Testing tasks.
+	grunt.registerMultiTask( 'phpunit', 'Runs PHPUnit tests, including the ajax and multisite tests.', function() {
+		grunt.util.spawn( {
+			cmd:  this.data.cmd,
+			args: this.data.args,
+			opts: { stdio: 'inherit' }
+		}, this.async() );
+	});
+
+	grunt.registerTask( 'test', 'Runs all unit tasks.', [ 'phpunit' ] );
 
 	// Default task.
-	grunt.registerTask('default', ['build-dev']);
+	grunt.registerTask( 'default', [ 'build' ] );
+
+	// Add a listener to the watch task.
+	//
+	// On `watch:all`, automatically updates the `copy:dynamic` and `clean:dynamic` configurations so that only the changed files are updated.
+	// On `watch:rtl`, automatically updates the `cssjanus:dynamic` configuration.
+	grunt.event.on( 'watch', function( action, filepath, target ) {
+		if ( target !== 'all' && target !== 'rtl' ) {
+			return;
+		}
+
+		var relativePath = path.relative( SOURCE_DIR, filepath ),
+		cleanSrc = ( action === 'deleted' ) ? [ relativePath ] : [],
+		copySrc  = ( action === 'deleted' ) ? [] : [ relativePath ];
+
+		grunt.config( [ 'clean', 'dynamic', 'src' ], cleanSrc );
+		grunt.config( [ 'copy', 'dynamic', 'src' ], copySrc );
+		grunt.config( [ 'cssjanus', 'dynamic', 'src' ], copySrc );
+	});
 };
