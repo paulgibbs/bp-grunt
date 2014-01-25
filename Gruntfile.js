e97e44d325d5757304f870c14f1e7257e61bc521
Paul Gibbs <paul@byotos.com>
2014-01-25 11:42:36 +0100

    Initial import, folder/structure setup

---
+++ Gruntfile.js
@@ -0,0 +1,162 @@
+/*global module:false*/
+module.exports = function(grunt) {
+	var path = require('path');
+	var SOURCE_DIR = 'src/';
+	var BUILD_DIR = 'build/';
+
+	// Load tasks.
+	require('matchdep').filterDev('grunt-*').forEach( grunt.loadNpmTasks );
+
+	// Project configuration.
+	grunt.initConfig({
+		clean: {
+			all: [BUILD_DIR],
+			build: [BUILD_DIR + 'templates/achievements/css/dev/',
+							BUILD_DIR + 'includes/admin/css/dev/'],
+			dynamic: {
+				cwd: BUILD_DIR,
+				dot: true,
+				expand: true,
+				src: [],
+			}
+		},
+		copy: {
+			all: {
+				files: [
+					{
+						cwd: SOURCE_DIR,
+						dest: BUILD_DIR,
+						dot: true,
+						expand: true,
+						src: ['!**/.{svn,git}/**','**'], // Ignore version control directories.
+					}
+				]
+			},
+			dynamic: {
+				cwd: SOURCE_DIR,
+				dest: BUILD_DIR,
+				dot: true,
+				expand: true,
+				src: [],
+			}
+		},
+		less: {
+			core: {
+				files: {
+					// front-end
+					'src/templates/achievements/css/achievements.css':  'src/templates/achievements/css/dev/achievements.less',
+
+					// admin
+					'src/includes/admin/css/achievements.css':     'src/includes/admin/css/dev/achievements.less',
+					'src/includes/admin/css/admin-editindex.css':  'src/includes/admin/css/dev/admin-editindex.less',
+					'src/includes/admin/css/chosen.css':           'src/includes/admin/css/dev/chosen.less',
+					'src/includes/admin/css/supportedplugins.css': 'src/includes/admin/css/dev/supportedplugins.less',
+					'src/includes/admin/css/users.css':            'src/includes/admin/css/dev/users.less',
+				}
+			}
+		},
+		cssmin: {
+			ltr: {
+				cwd: SOURCE_DIR,
+				dest: BUILD_DIR,
+				expand: true,
+				ext: '.css',
+				src: [
+					'templates/achievements/css/achievements.css',
+					'includes/admin/css/{achievements,admin-editindex,chosen,supportedplugins,users}.css'
+				],
+				options: {
+					banner: '/*! http://wordpress.org/plugins/achievements/ */'
+				}
+			},
+			rtl: {
+				cwd: BUILD_DIR,
+				dest: BUILD_DIR,
+				expand: true,
+				ext: '.css',
+				src: [
+					// RTL from cssjanus
+					'templates/achievements/css/achievements-rtl.css',
+					'includes/admin/css/{achievements,admin-editindex,chosen,supportedplugins,users}-rtl.css'
+				],
+				options: {
+					banner: '/*! http://wordpress.org/plugins/achievements/ */'
+				}
+			}
+		},
+		cssjanus: {
+			core: {
+				expand: true,
+				cwd: SOURCE_DIR,
+				dest: BUILD_DIR,
+				ext: '-rtl.css',
+				src: [
+					'templates/achievements/css/achievements.css',
+					'includes/admin/css/{achievements,admin-editindex,chosen,supportedplugins,users}.css'
+				],
+				options: {
+					generateExactDuplicates: true
+				}
+			}
+		},
+		uglify: {
+			core: {
+				cwd: SOURCE_DIR,
+				dest: BUILD_DIR,
+				expand: true,
+				ext: '.js',
+				src: [
+					'includes/admin/js/achievements.js',
+					'includes/admin/js/supportedplugins.js',
+					'templates/achievements/js/*.js'
+				],
+			},
+			options: {
+				banner: '/*! http://wordpress.org/plugins/achievements/ */\n'
+			}
+		},
+		phpunit: {
+			all: {
+				dir: 'tests/phpunit/'
+			}
+		},
+		watch: {
+			css: {
+				files: ["src/templates/achievements/css/dev/*",
+								"src/includes/admin/css/dev/*"],
+				tasks: ["less", "rsync:everything"],
+				options: {
+					spawn: false
+				}
+			},
+			sync: {
+				files: ['src/**'],
+				tasks: ["rsync:everything"],
+				options: {
+					spawn: false
+				}
+			}
+		},
+		rsync: {
+			options: {
+				exclude: ['.git*', '.DS_Store*', '._*', '.Spotlight*', '.Trashes*', '*thumbs.db*'],
+				recursive: true,
+				spawn: false,
+				syncDestIgnoreExcl: true
+			},
+			everything: {
+				options: {
+					src: "src/",
+					dest: process.env.ACHIEVEMENTS_DIR
+				}
+			}
+		}
+	});
+
+	// Register tasks.
+	grunt.registerTask('build-dev',  ['clean:all', 'less:core']);
+	grunt.registerTask('build-prod', ['clean:all', 'less:core', 'copy:all', 'cssjanus:core', 'cssmin:ltr', 'cssmin:rtl', 'uglify:core', 'clean:build', 'phpunit:all']);
+
+	// Default task.
+	grunt.registerTask('default', ['build-dev']);
+};
